d004864def8be9ee40ca2cd3a908d1c13bff063f
Andy Chen <acchen@cpp.edu>
2021-12-06 08:42:42 +0000

    added split line function

--- includes/loop.h
+++ includes/loop.h
@@ -4,6 +4,12 @@
 void lsh_loop(void);
 
 #define LSH_RL_BUFSIZE 1024
+
 char *lsh_read_line(void);
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+char** lsh_spllit_line(char *line);
 
 #endif
--- scr/loop.c
+++ scr/loop.c
@@ -1,6 +1,7 @@
 #include "../includes/loop.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void lsh_loop(void)
 {
@@ -11,11 +12,14 @@ void lsh_loop(void)
    do
    {
       printf("> ");
+      line = lsh_read_line();
+      args;
+      status;
 
-
-
-
-   } while (status);
+      free(line);
+      free(args);
+   } 
+   while (status);
    
 }
 
@@ -61,3 +65,38 @@ char* lsh_read_line(void)
       }
    }
 }
+
+char **lsh_split_line(char *line)
+   {
+      int bufsize = LSH_TOK_BUFSIZE, position = 0;
+      char **tokens = malloc(bufsize * sizeof(char*));
+      char *token;
+
+      if (!tokens)
+      {
+         fprintf(stderr, "lsh: allocation error\n");
+         exit(EXIT_FAILURE);
+      }
+
+      token = strtok(line, LSH_TOK_DELIM);
+      while (token != NULL)
+      {
+         tokens[position] = token;
+         position++;
+
+         if (position >= bufsize)
+         {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens)
+            {
+               fprintf(stderr, "lsh: allocation error\n");
+               exit(EXIT_FAILURE);
+            }
+         }
+
+         token = strtok(NULL, LSH_TOK_DELIM);
+      }
+      tokens[position] = NULL;
+      return tokens;
+   }
