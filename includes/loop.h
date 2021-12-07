4eb8cdc243f5968062baed8184c9f516f64d2802
Andy Chen <acchen@cpp.edu>
2021-12-07 02:12:13 +0000

    finished launch function

--- includes/loop.h
+++ includes/loop.h
@@ -12,4 +12,6 @@ char* lsh_read_line(void);
 
 char** lsh_split_line(char *line);
 
+int lsh_launch(char **args);
+
 #endif
--- scr/loop.c
+++ scr/loop.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 void lsh_loop(void)
 {
@@ -67,36 +69,67 @@ char* lsh_read_line(void)
 }
 
 char** lsh_split_line(char *line)
+{
+   int bufsize = LSH_TOK_BUFSIZE, position = 0;
+   char **tokens = malloc(bufsize * sizeof(char*));
+   char *token;
+
+   if (!tokens)
    {
-      int bufsize = LSH_TOK_BUFSIZE, position = 0;
-      char **tokens = malloc(bufsize * sizeof(char*));
-      char *token;
-
-      if (!tokens)
-      {
-         fprintf(stderr, "lsh: allocation error\n");
-         exit(EXIT_FAILURE);
-      }
-
-      token = strtok(line, LSH_TOK_DELIM);
-      while (token != NULL)
-      {
-         tokens[position] = token;
-         position++;
-
-         if (position >= bufsize)
-         {
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens)
-            {
-               fprintf(stderr, "lsh: allocation error\n");
-               exit(EXIT_FAILURE);
-            }
-         }
-
-         token = strtok(NULL, LSH_TOK_DELIM);
-      }
-      tokens[position] = NULL;
-      return tokens;
+      fprintf(stderr, "lsh: allocation error\n");
+      exit(EXIT_FAILURE);
    }
+
+   token = strtok(line, LSH_TOK_DELIM);
+   while (token != NULL)
+   {
+      tokens[position] = token;
+      position++;
+
+      if (position >= bufsize)
+      {
+         bufsize += LSH_TOK_BUFSIZE;
+         tokens = realloc(tokens, bufsize * sizeof(char*));
+         if (!tokens)
+         {
+            fprintf(stderr, "lsh: allocation error\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+
+      token = strtok(NULL, LSH_TOK_DELIM);
+   }
+   tokens[position] = NULL;
+   return tokens;
+}
+
+int lsh_launch(char **args)
+{
+   pid_t pid, wpid;
+   int status;
+
+
+   pid = fork();
+   if (pid == 0)
+   {
+      if (execvp(args[0], args) == -1)
+      {
+         perror("lsh");
+      }
+      exit(EXIT_FAILURE);
+   }
+   else if (pid < 0)
+   {
+      perror("lsh");
+   }
+   else
+   {
+      do
+      {
+         wpid = waitpid(pid, &status, WUNTRACED);
+      } 
+      while (!WIFEXITED(status && !WIFSIGNALED(status)));
+   }
+
+   return 1;
+}
